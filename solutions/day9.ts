267b237f227c1fe1ec2060a08d65cccac451dff1
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2024-12-08 23:59:10 -0600

    Solved day 9

---
+++ solutions/day9.ts
@@ -0,0 +1,77 @@
+// import BigNumber from "bignumber.js";
+import BigNumber from "bignumber.js";
+import { readFileSync } from "fs";
+
+const data = readFileSync("./data/day9.txt")
+  .toString()
+  .split("")
+  .map((n) => +n);
+
+let id = 0;
+let block: number[] = [];
+let file = true;
+data.forEach((n) => {
+  if (file) {
+    block.push(...Array(n).fill(id++));
+  } else {
+    block.push(...Array(n).fill(-1));
+  }
+  file = !file;
+});
+
+let last = block.length - 1;
+let newBlock = [...block];
+for (let i = 0; i < last; i++) {
+  if (newBlock[i] > -1) continue;
+  for (let j = last; j > i; j--) {
+    if (newBlock[j] > -1) {
+      newBlock[i] = newBlock[j];
+      newBlock[j] = -1;
+      last = j - 1;
+      break;
+    }
+  }
+}
+
+let checksum = BigNumber(0);
+for (let i = 0; i < newBlock.length && newBlock[i] > -1; i++) {
+  checksum = checksum.plus(BigNumber(i).times(newBlock[i]));
+}
+
+console.log(checksum.toFixed());
+
+newBlock = [];
+let files: (number | number[])[] = data.map((n, idx) =>
+  idx % 2 === 0 ? Array(n).fill(idx / 2) : n
+);
+
+for (let i = files.length - 1; i >= 0; i--) {
+  const last = files[i];
+  if (!Array.isArray(last)) continue;
+  for (let j = 0; j < i; j++) {
+    const first = files[j];
+    if (Array.isArray(first)) continue;
+    if (first >= last.length) {
+      let newEmpty = files[i - 1];
+      if (!Array.isArray(newEmpty)) {
+        files[i - 1] = newEmpty + last.length;
+      }
+      files[j] = first - last.length;
+      files.splice(i, 1);
+      files.splice(j, 0, last);
+      break;
+    }
+  }
+}
+
+newBlock = files.flatMap((file) =>
+  Array.isArray(file) ? file : Array(file).fill(-1)
+);
+
+checksum = BigNumber(0);
+for (let i = 0; i < newBlock.length; i++) {
+  if (newBlock[i] > -1) {
+    checksum = checksum.plus(BigNumber(i).times(newBlock[i]));
+  }
+}
+console.log(checksum.toFixed());
