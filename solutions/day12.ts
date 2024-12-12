9a2db9f78d5709e18d8e63ff91b05a429616b49c
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2024-12-12 12:27:28 -0600

    Improved day 12 part a

--- solutions/day12.ts
+++ solutions/day12.ts
@@ -9,41 +9,38 @@ const data = readFileSync("./data/day12.txt")
 const hash = (x: number, y: number) => y * data[0].length + x;
 
 const seen = new Set<number>();
-interface Price {
-  area: number;
-  perimeter: number;
-}
-const findPrice = (x: number, y: number, expected: string): Price => {
+const findPrice = (
+  x: number,
+  y: number,
+  expected: string,
+  price: {
+    area: number;
+    perimeter: number;
+  }
+): void => {
   if (seen.has(hash(x, y)) || data[y]?.[x] !== expected) {
-    return { area: 0, perimeter: 0 };
+    return;
   }
   seen.add(hash(x, y));
-  const above = findPrice(x, y - 1, expected),
-    below = findPrice(x, y + 1, expected),
-    left = findPrice(x - 1, y, expected),
-    right = findPrice(x + 1, y, expected);
 
-  const exposedPerimeter =
+  price.area++;
+  price.perimeter +=
     (data[y - 1]?.[x] !== expected ? 1 : 0) +
     (data[y + 1]?.[x] !== expected ? 1 : 0) +
     (data[y]?.[x - 1] !== expected ? 1 : 0) +
     (data[y]?.[x + 1] !== expected ? 1 : 0);
 
-  return {
-    area: 1 + above.area + below.area + left.area + right.area,
-    perimeter:
-      exposedPerimeter +
-      above.perimeter +
-      below.perimeter +
-      left.perimeter +
-      right.perimeter,
-  };
+  findPrice(x, y - 1, expected, price);
+  findPrice(x, y + 1, expected, price);
+  findPrice(x - 1, y, expected, price);
+  findPrice(x + 1, y, expected, price);
 };
 let totalPrice = BigNumber(0);
 
 for (let y = 0; y < data.length; y++) {
   for (let x = 0; x < data.length; x++) {
-    const price = findPrice(x, y, data[y][x]);
+    const price = { area: 0, perimeter: 0 };
+    findPrice(x, y, data[y][x], price);
     totalPrice = totalPrice.plus(BigNumber(price.area).times(price.perimeter));
   }
 }
