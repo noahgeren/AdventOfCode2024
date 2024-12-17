904b6c75e6d59745bdd3fd98f98bbe9fc58f42f0
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2024-12-16 23:54:29 -0600

    More attempts at 15b

--- solutions/day15.ts
+++ solutions/day15.ts
@@ -1,9 +1,9 @@
-import { readFileSync } from "fs";
-// import PromptSync from "prompt-sync";
 import BigNumber from "bignumber.js";
+import { readFileSync } from "fs";
+import PromptSync from "prompt-sync";
 import { Vector2d } from "../utilities/constants";
 
-// const prompt = PromptSync();
+const prompt = PromptSync();
 const data = readFileSync("./data/day15.txt").toString().split("\n\n");
 const initialMap = data[0].split("\n").map((row) => row.split(""));
 const instructions = data[1].split(/\n|/g) as ("<" | ">" | "^" | "v")[];
@@ -86,42 +86,82 @@ position = { x: initialPosition.x * 2, y: position.y };
 moveBox = (box, direction): boolean => {
 	if (map[box.y][box.x] === "#") {
 		return false;
+	} else if (map[box.y][box.x] === ".") {
+		return true;
 	}
+
 	if (direction.x === 0) {
 		// up/down - [
-		switch (map[box.y][box.x]) {
-			case ".":
-				break;
-			case "]":
-				break;
-			case "[":
-				break;
-		}
+		// // TODO: This is working for some edge cases
+		// switch (map[box.y][box.x]) {
+		// case "]":
+		// 	return (
+		// 		moveBox({ x: box.x, y: box.y + direction.y }, direction) &&
+		// 		moveBox({ x: box.x - 1, y: box.y }, direction)
+		// 	);
+		// case "[":
+		// 	if (
+		// 		moveBox(
+		// 			{ x: box.x + 1, y: box.y + direction.y },
+		// 			direction
+		// 		) &&
+		// 		moveBox({ x: box.x, y: box.y + direction.y }, direction)
+		// 	) {
+		// 		map[box.y + direction.y][box.x] = "[";
+		// 		map[box.y + direction.y][box.x + 1] = "]";
+		// 		map[box.y][box.x] = ".";
+		// 		map[box.y][box.x + 1] = ".";
+		// 		return true;
+		// 	}
+		// }
 	} else if (direction.x > 0) {
 		// right - [
 		switch (map[box.y][box.x]) {
-			case ".":
-				break;
 			case "]":
-				break;
+				return moveBox({ x: box.x + 1, y: box.y }, direction);
 			case "[":
-				break;
+				if (moveBox({ x: box.x + 1, y: box.y }, direction)) {
+					map[box.y][box.x + 1] = "[";
+					map[box.y][box.x + 2] = "]";
+					map[box.y][box.x] = ".";
+					return true;
+				}
 		}
 	} else {
 		// left - ]
 		switch (map[box.y][box.x]) {
-			case ".":
-				break;
 			case "]":
+				if (moveBox({ x: box.x - 1, y: box.y }, direction)) {
+					map[box.y][box.x - 1] = "]";
+					map[box.y][box.x - 2] = "[";
+					map[box.y][box.x] = ".";
+					return true;
+				}
 				break;
 			case "[":
-				break;
+				return moveBox({ x: box.x - 1, y: box.y }, direction);
 		}
 	}
 	return false;
 };
 
-for (const inst of instructions) {
+// map[position.y][position.x] = "@";
+// console.log(map.map((row) => row.join("")).join("\n"));
+// map[position.y][position.x] = ".";
+// prompt("press enter to continue: ");
+
+for (const [idx, inst] of instructions.entries()) {
+	// if (idx > 1380) {
+	// 	process.stdout.write("\x1Bc");
+	// 	map[position.y][position.x] = "@";
+	// 	console.log(`${inst} (${idx} / ${instructions.length})`);
+	// 	console.log(map.map((row) => row.join("")).join("\n"));
+	// 	map[position.y][position.x] = ".";
+	// 	await setTimeout(150);
+	// 	if (prompt("press enter to continue: ") === "exit") {
+	// 		break;
+	// 	}
+	// }
 	const direction = DIRECTIONS[inst];
 	const newPosition = {
 		x: position.x + direction.x,
@@ -133,13 +173,7 @@ for (const inst of instructions) {
 		case "#":
 			continue;
 	}
-	const horizontalBox =
-		(inst === "<" || inst === ">") && moveBox(newPosition, direction);
-	const verticalBox =
-		(inst === "^" || inst === "v") &&
-		moveBox(newPosition, direction) &&
-		moveBox({ x: newPosition.x - 1, y: newPosition.y }, direction);
-	if (horizontalBox || verticalBox) {
+	if (moveBox(newPosition, direction)) {
 		position = newPosition;
 	}
 }
@@ -152,4 +186,5 @@ for (let y = 0; y < map.length; y++) {
 		}
 	}
 }
+
 console.log(sum.toFixed());
