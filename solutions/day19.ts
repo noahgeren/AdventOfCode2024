f1f6a9f068a47da755e5cbfcd480bc9f6ebd3691
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2024-12-18 23:28:19 -0600

    Solved day 19

---
+++ solutions/day19.ts
@@ -0,0 +1,32 @@
+import BigNumber from "bignumber.js";
+import { readFileSync } from "fs";
+import { ONE, ZERO } from "../utilities/constants";
+
+const patternsWanted = readFileSync("./data/day19.txt")
+	.toString()
+	.split("\n")
+	.filter((str) => str.trim().length);
+const availableTowels = patternsWanted.shift()!.split(", ");
+
+const patternMemory = new Map<string, BigNumber>();
+const waysToMakePattern = (pattern: string): BigNumber => {
+	if (!pattern.length) return ONE;
+	if (patternMemory.has(pattern)) {
+		return patternMemory.get(pattern)!;
+	}
+	let waysToMake = ZERO;
+	for (const towel of availableTowels) {
+		if (pattern.startsWith(towel)) {
+			waysToMake = waysToMake.plus(
+				waysToMakePattern(pattern.substring(towel.length))
+			);
+		}
+	}
+	patternMemory.set(pattern, waysToMake);
+	return waysToMake;
+};
+
+const patternCounts = patternsWanted.map(waysToMakePattern);
+
+console.log(patternCounts.filter((n) => !n.isZero()).length);
+console.log(patternCounts.reduce((a, b) => a.plus(b), ZERO));
