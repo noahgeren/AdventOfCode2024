5c86758995ae01ebb3edc2c18be1b23ffd0c157d
Noah Geren <noah.s.geren@gmail.com>
2024-12-22 19:06:42 -0600

    Solved part 2 of day 22

--- solutions/day22.ts
+++ solutions/day22.ts
@@ -1,9 +1,11 @@
 import BigNumber from "bignumber.js";
 import { readFileSync } from "fs";
 import { bitwiseXOR, TWO, ZERO } from "../utilities/constants";
+import HashMap from "../utilities/HashMap";
 
 const ITERATIONS = 2000;
 const MODULUS = BigNumber("16777216");
+const TEN = BigNumber(10);
 const THIRTY_TWO = TWO.pow(5);
 const SIXTY_FOUR = TWO.pow(6);
 const TWO_KIBI = TWO.pow(11);
@@ -12,18 +14,46 @@ const secretNumbers = readFileSync("./data/day22.txt")
 	.split("\n")
 	.map((n) => BigNumber(n));
 
+type PatternKey = [number, number, number, number];
+
+const patternHashFn = (key: PatternKey) => {
+	let hash = 0;
+	hash = (hash * 20) + key[0];
+	hash = (hash * 20) + key[1];
+	hash = (hash * 20) + key[2];
+	hash = (hash * 20) + key[3];
+	return hash;
+};
+
+const patternTotals = new HashMap<PatternKey, number>(patternHashFn);
+
 const finalNumbers = secretNumbers.map((secret, idx) => {
-	console.log(idx);
+	const pattern: number[] = []; 
+	let previousPrice = secret.mod(TEN).toNumber();
+	const patternMaxes = new HashMap<PatternKey, number>(patternHashFn);
 	for (let i = 0; i < ITERATIONS; i++) {
 		secret = bitwiseXOR(secret, secret.times(SIXTY_FOUR)).mod(MODULUS);
 		secret = bitwiseXOR(secret, secret.dividedToIntegerBy(THIRTY_TWO)).mod(
 			MODULUS
 		);
 		secret = bitwiseXOR(secret, secret.times(TWO_KIBI)).mod(MODULUS);
+		const currentPrice = secret.mod(TEN).toNumber()
+		pattern.push(currentPrice - previousPrice);
+		previousPrice = currentPrice;
+		if(pattern.length === 4) {
+			if(!patternMaxes.has(pattern as PatternKey)) {
+				patternMaxes.set([...pattern] as PatternKey, currentPrice);
+			}
+			pattern.shift();
+		}
 	}
+	patternMaxes.forEach((maxPrice, pattern) => {
+		const currentTotal = patternTotals.get(pattern) ?? 0;
+		patternTotals.set([...pattern], currentTotal + maxPrice);
+	});
 	return secret;
 });
 
 console.log(finalNumbers.reduce((a, b) => a.plus(b), ZERO));
 
-// TODO: Can I bruteforce part 2 also?
+console.log(Math.max(...patternTotals.values()))
