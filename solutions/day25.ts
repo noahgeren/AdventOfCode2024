d67b79f6e2b1340f769a0628cd2294b96996cc4d
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2024-12-24 23:30:05 -0600

    Solved day 25

---
+++ solutions/day25.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from "fs";
+
+const data = readFileSync("./data/day25.txt").toString().split("\n\n");
+
+const height = data[0].split("\n").length;
+const locks: number[][] = [];
+const keys: number[][] = [];
+
+data.forEach((item) => {
+	const lines = item.split("\n");
+	const columns = Array<number>(lines[0].length).fill(0);
+	lines.forEach((line) =>
+		line.split("").forEach((char, idx) => {
+			if (char === "#") {
+				columns[idx]++;
+			}
+		})
+	);
+	if (item.startsWith("#")) {
+		locks.push(columns);
+	} else {
+		keys.push(columns);
+	}
+});
+
+let sum = 0;
+for (const lock of locks) {
+	for (const key of keys) {
+		if (lock.every((pin, idx) => pin + key[idx] <= height)) {
+			sum++;
+		}
+	}
+}
+console.log(sum);
