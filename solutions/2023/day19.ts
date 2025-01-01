9862dd9bf2e10242c40a43bd898ce33f3ba0cba1
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2024-12-31 18:58:20 -0600

    Solved part 2 of 2023 day 19

--- solutions/2023/day19.ts
+++ solutions/2023/day19.ts
@@ -1,37 +1,38 @@
+import { ONE, ZERO } from "#/utilities/constants";
 import { readInputFile } from "#/utilities/general";
+import BigNumber from "bignumber.js";
 
 const data = readInputFile(2023, 19).split("\n\n");
 interface Rule {
-	operand?: string;
+	operand?: "x" | "m" | "a" | "s";
 	operator?: "<" | ">";
 	value?: number;
 	result: string;
 }
-const workflows: Record<string, Rule[]> = data[0]
-	.split("\n")
-	.reduce((tempWorkflows, row) => {
+const workflows: Record<string, Rule[]> = data[0].split("\n").reduce(
+	(tempWorkflows, row) => {
 		const [name, rules] = row.split("{");
-		return {
-			...tempWorkflows,
-			[name]: rules
-				.replace("}", "")
-				.split(",")
-				.map((rule) => {
-					const tokens = rule.split(":");
-					if (tokens.length === 1) {
-						return { result: tokens[0] };
-					}
-					const operator = tokens[0].includes("<") ? "<" : ">";
-					const [operand, value] = tokens[0].split(operator);
-					return {
-						operand,
-						operator,
-						value: +value,
-						result: tokens[1]
-					};
-				})
-		};
-	}, {});
+		tempWorkflows[name] = rules
+			.replace("}", "")
+			.split(",")
+			.map((rule) => {
+				const tokens = rule.split(":");
+				if (tokens.length === 1) {
+					return { result: tokens[0] };
+				}
+				const operator = tokens[0].includes("<") ? "<" : ">";
+				const [operand, value] = tokens[0].split(operator);
+				return {
+					operand: operand as Rule["operand"],
+					operator,
+					value: +value,
+					result: tokens[1]
+				};
+			});
+		return tempWorkflows;
+	},
+	{} as Record<string, Rule[]>
+);
 
 const ratings = data[1].split("\n").map((row) => {
 	const rating: Record<string, number> = {};
@@ -62,9 +63,70 @@ const totalOfAcceptableRatings = ratings
 	.filter(testRating)
 	.reduce(
 		(total, rating) =>
-			total + Object.values(rating).reduce((a, b) => a + b, 0),
-		0
+			total.plus(Object.values(rating).reduce((a, b) => a + b, 0)),
+		ZERO
 	);
-console.log(totalOfAcceptableRatings);
+console.log(totalOfAcceptableRatings.toFixed());
 
-// TODO: Solve part 2 by finding all ranges that are/aren't acceptable
+const oneToFourThousand = Object.keys([...Array(4000)]).map((n) => +n + 1);
+const allPossibleValues = {
+	x: [...oneToFourThousand],
+	m: [...oneToFourThousand],
+	a: [...oneToFourThousand],
+	s: [...oneToFourThousand]
+};
+
+const findAcceptableCombinations = (
+	workflowId: string,
+	possibleValues: typeof allPossibleValues
+): BigNumber => {
+	if (
+		workflowId === "R" ||
+		Object.values(possibleValues).some(
+			(possibleRatings) => !possibleRatings.length
+		)
+	) {
+		return ZERO;
+	} else if (workflowId === "A") {
+		return Object.values(possibleValues).reduce(
+			(a, b) => a.times(b.length),
+			ONE
+		);
+	}
+
+	let totalCombinations = ZERO;
+	for (const rule of workflows[workflowId]) {
+		if (!rule.operand || !rule.operator || rule.value === undefined) {
+			totalCombinations = totalCombinations.plus(
+				findAcceptableCombinations(rule.result, possibleValues)
+			);
+		} else {
+			totalCombinations = totalCombinations.plus(
+				findAcceptableCombinations(rule.result, {
+					...possibleValues,
+					[rule.operand]: possibleValues[rule.operand].filter(
+						(testValue) => {
+							return rule.operator === "<"
+								? testValue < rule.value!
+								: testValue > rule.value!;
+						}
+					)
+				})
+			);
+			possibleValues = {
+				...possibleValues,
+				[rule.operand]: possibleValues[rule.operand].filter(
+					(testValue) => {
+						return rule.operator === "<"
+							? testValue >= rule.value!
+							: testValue <= rule.value!;
+					}
+				)
+			};
+		}
+	}
+
+	return totalCombinations;
+};
+
+console.log(findAcceptableCombinations("in", allPossibleValues).toFixed());
